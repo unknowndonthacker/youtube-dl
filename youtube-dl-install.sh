6f720c3be6e5c5eedb588097de21d5ffba063ad7
unknowndonthacker <67613775+unknowndonthacker@users.noreply.github.com>
2020-06-29 21:38:56 -0300

    Add files via upload

---
+++ youtube-dl-install.sh
@@ -0,0 +1,8 @@
+sudo apt-get install youtube-dl -y
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod +x /usr/local/bin/youtube-dl
+sudo mv /usr/local/bin/youtube-dl /usr/bin/
+sudo youtube-dl -U
+echo "instalação finalizada com sucesso"
+mkdir ~/.config/youtube-dl
+touch ~/.config/youtube-dl/config
