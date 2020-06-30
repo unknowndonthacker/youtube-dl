876b786e7d915d53857dad43a84f2ad1de7f408d
unknowndonthacker <67613775+unknowndonthacker@users.noreply.github.com>
2020-06-29 22:13:24 -0300

    Update youtube-dl-install.sh

--- youtube-dl-install.sh
+++ youtube-dl-install.sh
@@ -1,4 +1,5 @@
-sudo apt-get install youtube-dl -y
+#!/bin/bash
+sudo apt-get install curl youtube-dl -y
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod +x /usr/local/bin/youtube-dl
 sudo mv /usr/local/bin/youtube-dl /usr/bin/
@@ -7,3 +8,4 @@ echo "instalação finalizada com sucesso"
 mkdir ~/.config/youtube-dl
 touch ~/.config/youtube-dl/config
 cp config ~/.config/youtube-dl/
+echo "terminado"
